622ab774cb979b82321c5fc1b3af823400cb1a05
luc wastiaux <luc.wastiaux@airpost.net>
2020-08-17 09:41:25 +0800

    scan for eligible models

--- __init__.py
+++ __init__.py
@@ -7,7 +7,6 @@ from aqt.utils import showInfo
 import aqt.utils
 import anki.lang
 # import all of the Qt GUI library
-#from aqt.qt import *
 import aqt.importing
 from anki import hooks
 from aqt import gui_hooks
@@ -24,6 +23,11 @@ class SmartTextImporter(anki.importing.csvfile.TextImporter):
         header_note = notes[0]
         notes = notes[1:]
         return notes
+
+    def getHeaderRow(self):
+        notes = super().foreignNotes()
+        header_note = notes[0]
+        return header_note
     
 
 def smartImport():
@@ -45,7 +49,41 @@ def smartImport():
     note = note_list[0]
     first_field = note.fields[0]
 
-    aqt.utils.showInfo(str(first_field))
+    # get headers
+    header_note = importer.getHeaderRow()
+
+    # aqt.utils.showInfo(str(first_field))
+    # aqt.utils.showInfo(f"columns: {', '.join(header_note.fields)}")
+
+    # look at existing models
+    # mw.col.models <-- this is a ModelManager
+
+    eligible_models = []
+
+    all_models = mw.col.models.all()
+    for model in all_models:
+        # print(str(model))
+        model_id = model['id']
+        model_name = model['name']
+        model_fields = model['flds']
+
+        field_names = [x['name'] for x in model_fields]
+
+        # iterate over the header note
+        is_eligible = True
+        for wanted_field in header_note.fields:
+            if wanted_field not in field_names:
+                # field not found
+                is_eligible = False
+        
+        if is_eligible:
+            print(f"model is eligible: {model_name}")
+            eligible_models.append({'model_name': model_name, 'model_id': model_id})
+
+    print(f"found eligible models: {str(eligible_models)}")
+
+    
+
 
 
 # create a new menu item, "test"
